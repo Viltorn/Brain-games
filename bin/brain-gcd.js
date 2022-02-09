c020a3d141547139770eb9116b56071a6aec8901
Viltorn <viltorn@gmail.com>
2022-02-09 15:44:23 +0300

    add brain-progression.js, progression.js

---
+++ bin/brain-gcd.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import randomNumbersDevider from '../games/gcd.js';
+import { numberOfRounds } from '../src/index.js';
+
+randomNumbersDevider(numberOfRounds);
---
+++ bin/brain-progression.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import mathProgression from '../games/progression.js';
+import { numberOfRounds } from '../src/index.js';
+
+mathProgression(numberOfRounds);
